7e1f87e8d6b68d1fc5a242d35e6e23bff13eb7d0
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-10-13 00:23:20 +0530

    Create For Loop.js

---
+++ For Loop.js
@@ -0,0 +1,26 @@
+//calculate sum of 1 to 5
+
+let sum=0;
+let n=5         //or write direct value as i<=100
+for(let i=1; i<=n; i++){
+    sum=sum+i;
+}
+console.log("sum =", sum);
+console.log("loop has ended");
+
+
+
+
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+let i;
+console.log(i);        // will print 1 to 10
+
+
+
+
+//infinte loop
+for(let i=1; i<=5; i++){   //i>=0   browser crash / pc crash / job gone ( overkill 💀 )
+    console.log(i)
+}
