b0310e51b9739606a92644ba795df2870163743b
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-09 01:49:56 +0530

    Update 7. Var and Confirm.js

--- 7. Var and Confirm.js
+++ 7. Var and Confirm.js
@@ -1,5 +1,23 @@
+// Example 1
 console.log("hey there")
 var a=18;
 console.log(a)
 confirm("Wdym?")
 confirm("Choose Ok if you learnt anything today.")
+
+
+// Example 2
+let q2=confirm("Aur sab sahi??")
+if(q2){
+    console.log("Noice Bhai")
+}
+else{
+    console.log("Kyu?? Kya hua?")
+}
+
+console.log("1218")
+document.title="Hey I'm good."
+
+// after answering the prompt change color to pink
+document.body.style.backgroundColor="pink"
+
