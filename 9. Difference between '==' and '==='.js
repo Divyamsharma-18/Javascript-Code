994fefdd429c80c97bad0fd3fe4d4b6e2c3843c1
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-11 00:24:24 +0530

    Update 9. Difference between '==' and '==='.js

--- 9. Difference between '==' and '==='.js
+++ 9. Difference between '==' and '==='.js
@@ -1,8 +1,17 @@
 let a=10;
 let b="10";
-if(a===b){
-    console.log("a==b")
+
+if(a==b){
+    console.log("a==b")                // true cause it only checks if the data is same or not
 }
 else{
-    console.log("a!=b")
+    console.log("a!=b")                 
+}
+
+
+if(a===b){
+    console.log("a===b")                
+}
+else{
+    console.log("a!===b")                 // false cause the data is same but the data type is not
 }
