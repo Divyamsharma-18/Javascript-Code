f7d0c6a29dc4b4ab17cf68de2ce6cee8248da1de
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-12 00:47:23 +0530

    Create 10. Function basics.js

---
+++ 10. Function basics.js
@@ -0,0 +1,8 @@
+function main(name) {                                          //main is just a name
+    console.log("Hey " + name + " whats up?")
+    console.log("Hey " + name + " Your hoodie is nice!!")
+    console.log("Hey " + name + " Your code is good!!")
+    console.log("Hey " + name + " You're the best!!")
+}
+main("Divit")
+main("Divay")
