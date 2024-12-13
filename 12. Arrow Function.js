91915b4757933ac021f183b4636d965a8b0f260c
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-14 01:33:59 +0530

    Create 12. Arrow Function.js

---
+++ 12. Arrow Function.js
@@ -0,0 +1,18 @@
+// Example 1
+const func1=(x)=>{
+    console.log("I am an arrow function",x)
+}
+func1(5);
+func1(18);
+func1(5);
+func1(2);
+func1(29);
+
+
+// Example 2
+const name=(x)=>{
+    console.log("My name is", x)  //print many lines with same code
+}
+name("Divyam");
+name("Divit");
+name("Divay");
