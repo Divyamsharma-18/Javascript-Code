40efcb18855d44088645376b8e9b53163495ed89
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-17 00:45:17 +0530

    Create 14. String Basics and Manipulation.js

---
+++ 14. String Basics and Manipulation.js
@@ -0,0 +1,18 @@
+console.log("This is string");
+
+let a="Harry";        //obvio it's start from 0
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+// console.log(a[5])  offcourse undefined
+
+console.log(a.length);    //print length 
+
+let name="Divit"
+let friend="Divay"
+console.log("His name is " +name+ " and his friend's name is "+friend+ ".") 
+console.log(`His name is ${name} and his friend's name is ${friend}.`)
