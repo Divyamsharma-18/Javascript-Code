aacc511ff3ee9617dea35d2255cab7b14430b6aa
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-22 00:15:38 +0530

    Create 19. Reduce.js

---
+++ 19. Reduce.js
@@ -0,0 +1,11 @@
+//REDUCE as the name suggest sum, mul... to reduce the size of array
+
+let arr=[1,2,3,4,5]
+const red=(a,b)=>{            
+    return a*b                      //it'll yk * 
+}
+console.log(arr.reduce(red))
+
+let a= Array.from("Divyam"); 
+// or Array.from("Divyam");         //direct in browser, no need of var declaration
+console.log(a)
