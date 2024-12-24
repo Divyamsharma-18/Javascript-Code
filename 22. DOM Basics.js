197e9b8df87fb9884cd309d37328df5e9a876b9d
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-25 00:08:21 +0530

    Create 22. DOM Basics.js

---
+++ 22. DOM Basics.js
@@ -0,0 +1,14 @@
+// DOM
+document.title=("Divyam")
+console.log(document.title)
+console.log(document.body)                //shows body tag 
+document.body.childNodes
+let cont=document.body.childNodes[5]      //div.container
+cont.firstChild
+cont.lastChild
+cont.firstElementChild
+cont.lastElementChild
+cont.previousSibling
+cont.nextSibling
+cont.nextElementSibling
+cont.previousElementSibling
