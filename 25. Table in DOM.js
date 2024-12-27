24766af51e14127c6f4ce47654b116dbc3c2b9e2
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2024-12-28 00:01:39 +0530

    Create 25. Table in DOM.js

---
+++ 25. Table in DOM.js
@@ -0,0 +1,11 @@
+// table > tbody > tr*3 > th*3          (in html)
+
+// even if we don't write tbody, the browser will 
+
+document.body.children[1]
+document.body.children[1].rows
+document.body.children[1].contains
+document.body.children[1].tHead
+document.body.children[1].tFoot
+document.body.children[1].tBodies
+document.body.children[1].tbody.rows
